7ff1d754a417ebf4438b5dcdb81f9b9a593dd93e
Vladimir Gromakovskiy <gromakovskiyvladimir1989@gmail.com>
2019-01-14 02:18:29 +0300

    Пробный запуск_2

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 int main (int argc, char *argv[])
 {
     std::cout<<"Hello world"<<std::endl;
-    //auto logger = spdlog::stdout_logger_mt("console");
-    //logger->info("version {} was started", version());
+    auto logger = spdlog::stdout_logger_mt("console");
+    logger->info("version {} was started", version());
     std::system("pause");
     return 0;
 }
