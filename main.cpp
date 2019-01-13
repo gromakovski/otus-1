66fd74e818515ef64d731e8e761e955480a84aa4
Vladimir Gromakovskiy <gromakovskiyvladimir1989@gmail.com>
2019-01-14 02:17:50 +0300

    Пробный запуск

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "lib.h"
+
+#include <iostream>
+
+//#include <spdlog/spdlog.h>
+
+int main (int argc, char *argv[])
+{
+    std::cout<<"Hello world"<<std::endl;
+    //auto logger = spdlog::stdout_logger_mt("console");
+    //logger->info("version {} was started", version());
+    std::system("pause");
+    return 0;
+}
