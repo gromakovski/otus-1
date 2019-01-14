233fa1671fe125c8e37f8c90d1b9f56d420ee3dc
Vladimir Gromakovskiy <gromakovskiyvladimir1989@gmail.com>
2019-01-14 03:44:38 +0300

    Перенастроил bintray

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 int main (int argc, char *argv[])
 {
     std::cout<<"Hello world"<<std::endl;
+    std::cout<<"Hello otus"<<std::endl;
     auto logger = spdlog::stdout_logger_mt("console");
     logger->info("version {} was started", version());
     return 0;
