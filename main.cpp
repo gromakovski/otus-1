1e92006406a73f2347dfe3362aa71a46861c0dfd
Vladimir <46077799+gromakovski@users.noreply.github.com>
2019-01-29 09:32:41 +0300

    Удалил ненужный вывод

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 int main (int argc, char *argv[])
 {
     std::cout<<"Hello world"<<std::endl;
-    std::cout<<"Hello otus"<<std::endl;
     auto logger = spdlog::stdout_logger_mt("console");
     logger->info("version {} was started", version());
     return 0;
