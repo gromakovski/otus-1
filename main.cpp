296d5c2e68db559d33509219d7100dafcc510b20
Vladimir <46077799+gromakovski@users.noreply.github.com>
2019-02-16 18:14:01 +0300

    Эксперимент)

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 
 int main (int argc, char *argv[])
 {
-    std::cout<<"Hello world"<<std::endl;
+    std::cout<<"Hello Lesha"<<std::endl;
     auto logger = spdlog::stdout_logger_mt("console");
     logger->info("version {} was started", version());
     return 0;
 }
+
