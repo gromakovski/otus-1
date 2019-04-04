111557f045a45d1943f95f7236a82ca40f82f8d9
Vladimir <46077799+gromakovski@users.noreply.github.com>
2019-04-04 05:13:11 +0300

    Update test_main.cpp

--- test_main.cpp
+++ test_main.cpp
@@ -1,10 +1,13 @@
 #include <gtest/gtest.h>
 
+TEST(hwTest, TEST_EXAMPLE) {
+  EXPECT_EQ(1,1);
+}
 
 int main(int argc, char const *argv[])
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
 
